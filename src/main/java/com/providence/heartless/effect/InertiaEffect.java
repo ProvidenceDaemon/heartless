b99a2d086689739fc1ed1db61a588ec311a06351
ProvidenceDaemon <91506764+ProvidenceDaemon@users.noreply.github.com>
2026-06-16 20:38:13 -0500

    inertia effect is now incurable hearts are now kept on death

--- src/main/java/com/providence/heartless/effect/InertiaEffect.java
+++ src/main/java/com/providence/heartless/effect/InertiaEffect.java
@@ -8,8 +8,13 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import team.lodestar.lodestone.helpers.ColorHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InertiaEffect extends MobEffect {
     public InertiaEffect() {
@@ -18,6 +23,9 @@ public class InertiaEffect extends MobEffect {
         this.addAttributeModifier(Attributes.ATTACK_SPEED, "02807bd4-68d6-11f1-8cd6-325096b39f47", +0.01, AttributeModifier.Operation.MULTIPLY_BASE);
     }
 
-
-
+    @Override
+    public List<ItemStack> getCurativeItems() {
+        ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+        return ret;
+    }
 }
