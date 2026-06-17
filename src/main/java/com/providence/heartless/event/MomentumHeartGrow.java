da4aabab52b99bedc4853ad6d45284da43b0c33a
ProvidenceDaemon <91506764+ProvidenceDaemon@users.noreply.github.com>
2026-06-16 21:10:49 -0500

    add method for obtaining the momentum heart

---
+++ src/main/java/com/providence/heartless/event/MomentumHeartGrow.java
@@ -0,0 +1,24 @@
+package com.providence.heartless.event;
+
+import com.providence.heartless.Heartless;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import top.theillusivec4.curios.api.CuriosApi;
+
+@Mod.EventBusSubscriber(modid = Heartless.MOD_ID)
+public class MomentumHeartGrow {
+    @SubscribeEvent
+    public static void onLivingHurt(LivingHurtEvent event){
+        if(event.getEntity() instanceof Player && event.getSource().is(DamageTypes.FLY_INTO_WALL) && event.getAmount() >= 12){
+            CuriosApi.getCuriosInventory(event.getEntity()).ifPresent(curiosInventory -> {
+                if (curiosInventory.findCurio("heart", 0).isEmpty()) {
+                    curiosInventory.setEquippedCurio("heart", 0, new ItemStack(Heartless.MOMENTUM_HEART.get()));
+                }
+            });
+        }
+    };
+}
